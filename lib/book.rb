cd28eac0b9cf3cd4e033d058de12115e2c8ba30d
Charlotte Neff <charlotte.neff@gmail.com>
2019-04-08 22:57:25 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,16 @@
 class Book
-  attr_accessor :title, :author, :genre, :page_count
-  
+  attr_accessor :author, :page_count, :genre
+ 
   def initialize(title)
     @title = title
   end
-  
-  def turn_page
-    "Flipping the page...wow, you read fast!"
+ 
+  def title
+    @title
   end
-  
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
 end
